7d329d221472875b6976f250139e8e1eb8e86de1
Jebrcidea <jebrcidea@hotmail.com>
2020-07-13 17:23:12 -0500

    added webserver to weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -45,6 +45,22 @@ app.get("/weather", (req, res) => {
     })
 })
 
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: 'Jaime Beltrán',
+        errorMessage: "Help article not found."
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: 'Jaime Beltrán',
+        errorMessage: "Page not found."
+    })
+})
+
 
 app.listen(3000, () => {
     console.log("Server is up on port 3000")
